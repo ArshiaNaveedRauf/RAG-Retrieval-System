4c668c761d57000ec4478b6f83e0f0617c501466
ArshiaNaveedRauf <arshianaveed0786@gmail.com>
2026-09-14 18:43:52 +0500

    test

---
+++ tests/test_embedder.py
@@ -0,0 +1,17 @@
+from src.data.embedder import DataEmbedder
+import numpy as np
+from langchain_core.documents import Document
+
+def test_embedder():
+    chunks =[ Document( page_content="MMSE Lab is a research laboratory at NED University.", metadata={"source": "fake-website"} ),
+              Document( page_content="The laboratory focuses on research and technological innovation.", metadata={"source": "fake-website"} ),
+              Document( page_content="Students and researchers work together on different projects.", metadata={"source": "fake-website"} ) ]
+
+    embedder= DataEmbedder()
+    embeddings= embedder.embedding_generator(chunks)
+
+    assert isinstance(embeddings, np.ndarray)
+
+
+
+       
